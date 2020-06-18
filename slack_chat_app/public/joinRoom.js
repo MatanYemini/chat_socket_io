4629776a5128ada34345a8b7d3f8441827f5cfc7
Yemini <yeminimatan@gmail.com>
2020-06-18 17:01:02 +0300

    Adding final room and namespace configuration

--- slack_chat_app/public/joinRoom.js
+++ slack_chat_app/public/joinRoom.js
@@ -6,4 +6,35 @@ function joinRoom(roomName) {
       '.curr-room-num-users'
     ).innerHTML = `${newNumberMembers} <span class="glyphicon glyphicon-user"></span>`;
   });
+  nsSocket.on('historyCatchUp', (history) => {
+    const messagesUI = document.querySelector('#messages');
+    messagesUI.innerHTML = '';
+    history.forEach((msg) => {
+      const newMsg = buildHTML(msg);
+      const currentMessages = messagesUI.innerHTML;
+      messagesUI.innerHTML = currentMessages + newMsg;
+    });
+    messagesUI.scrollTo(0, messagesUI.scrollHeight);
+  });
+  nsSocket.on('updateMembers', (numMembers) => {
+    document.querySelector(
+      '.curr-room-num-users'
+    ).innerHTML = `${numMembers} <span class="glyphicon glyphicon-user"></span>`;
+    document.querySelector('.curr-room-text').innerHTML = `${roomName}`;
+  });
+
+  let searchBox = document.querySelector('#search-box');
+  searchBox.addEventListener('input', (e) => {
+    let messages = Array.from(document.getElementsByClassName('message-text'));
+    messages.forEach((msg) => {
+      if (
+        msg.innerText.toLowerCase().indexOf(e.target.value.toLowerCase()) === -1
+      ) {
+        // the msg does not contain the user search term!
+        msg.style.display = 'none';
+      } else {
+        msg.style.display = 'block';
+      }
+    });
+  });
 }
--- slack_chat_app/slack.js
+++ slack_chat_app/slack.js
@@ -1,64 +1,89 @@
 const express = require('express');
-const socketio = require('socket.io');
 const app = express();
+const socketio = require('socket.io');
 
 let namespaces = require('./data/namespaces');
-
-app.use(express, express.static(__dirname + '/public'));
-
+// console.log(namespaces[0]);
+app.use(express.static(__dirname + '/public'));
 const expressServer = app.listen(9000);
-const io = socketio(expressServer, {
-  path: '/socket.io',
-  serveClient: true,
-});
+const io = socketio(expressServer);
 
+// io.on = io.of('/').on = io.sockets.on
+// io.emit = io.of('/').emit = io.sockets.emit
 io.on('connection', (socket) => {
-  // build an array to send back with img and endpoint for each NS
+  // console.log(socket.handshake)
+  // build an array to send back with the img and endpoing for each NS
   let nsData = namespaces.map((ns) => {
     return {
       img: ns.img,
       endpoint: ns.endpoint,
     };
   });
-  // send namespace data back to the client, we need to use socket, NOT io, because we want it to go
-  // just to this client
+  // console.log(nsData)
+  // sned the nsData back to the client. We need to use socket, NOT io, because we want it to
+  // go to just this client.
   socket.emit('nsList', nsData);
 });
 
+// loop through each namespace and listen for a connection
 namespaces.forEach((namespace) => {
+  // const thisNs = io.of(namespace.endpoint)
   io.of(namespace.endpoint).on('connection', (nsSocket) => {
-    console.log(`${nsSocket.id} has join ${namespace.endpoint}`);
-    // someone connected to our chatgroups namespace
-    nsSocket.emit('nsRoomLoad', namespaces[0].rooms);
-    nsSocket.on('joinRoom', (roomToJoin, usersNumberCallBack) => {
+    const username = nsSocket.handshake.query.username;
+    // a socket has connected to one of our chatgroup namespaces.
+    // send that ns gorup info back
+    nsSocket.emit('nsRoomLoad', namespace.rooms);
+    nsSocket.on('joinRoom', (roomToJoin, numberOfUsersCallback) => {
+      // deal with history... once we have it
+      const roomToLeave = Object.keys(nsSocket.rooms)[1];
+      nsSocket.leave(roomToLeave);
+      updateUsersInRoom(namespace, roomToLeave);
       nsSocket.join(roomToJoin);
-      // A callback when someone is entered
-      //Deal with history
-      io.of('/wiki')
+      io.of(namespace.endpoint)
         .in(roomToJoin)
         .clients((error, clients) => {
-          console.log(clients);
-          usersNumberCallBack(clients.length);
+          numberOfUsersCallback(clients.length);
         });
+      const nsRoom = namespace.rooms.find((room) => {
+        return room.roomTitle === roomToJoin;
+      });
+      nsSocket.emit('historyCatchUp', nsRoom.history);
+      updateUsersInRoom(namespace, roomToJoin);
     });
     nsSocket.on('newMessageToServer', (msg) => {
       const fullMsg = {
         text: msg.text,
         time: Date.now(),
-        username: 'rbunch',
+        username: username,
         avatar: 'https://via.placeholder.com/30',
       };
-      console.log(msg);
-      // Send the message to All the sockets that are in the room that this Socket is in.
-      // how we can find what rooms THIS socket is in?
-      // The user will be in the 2nd room in the object list (this first is the namespace)
-      // This is because the socket ALWAYS joins its own room on connection
-      const roomTitle = Object.keys(nsSocket.rooms);
-      io.of('/wiki').to(roomTitle).emit('messageToClients', fullMsg);
+      // Send this message to ALL the sockets that are in the room that THIS socket is in.
+      // how can we find out what rooms THIS socket is in?
+      // console.log(nsSocket.rooms)
+      // the user will be in the 2nd room in the object list
+      // this is because the socket ALWAYS joins its own room on connection
+      // get the keys
+      const roomTitle = Object.keys(nsSocket.rooms)[1];
+      // we need to find the Room object for this room
+      const nsRoom = namespace.rooms.find((room) => {
+        return room.roomTitle === roomTitle;
+      });
+      nsRoom.addMessage(fullMsg);
+      io.of(namespace.endpoint).to(roomTitle).emit('messageToClients', fullMsg);
     });
   });
 });
 
+function updateUsersInRoom(namespace, roomToJoin) {
+  // Send back the number of users in this room to ALL sockets connected to this room
+  io.of(namespace.endpoint)
+    .in(roomToJoin)
+    .clients((error, clients) => {
+      io.of(namespace.endpoint)
+        .in(roomToJoin)
+        .emit('updateMembers', clients.length);
+    });
+}
 // Socket IO server -> Namespaces (localhost:9000/[namespace]) -> Romms are inside the namespace
 // Namespaces are not shared with others
 // Rooms are shared
